41cfc7d3d1a2d99b3cb529735d0097b10e751909
Talrejasaniya <brilliantart0927@gmail.com>
2026-03-27 23:58:52 +0530

    fixed database file

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 1. Variable uthayein
 db_url = os.getenv("DATABASE_URL")
-
+if db_url:
+    host_part = db_url.split('@')[-1] if '@' in db_url else "MISSING HOST"
+    print(f"DEBUG: Connecting to host -> {host_part}")
 # 🧐 Saniya, ye check karega ki URL mila ya nahi
 if db_url is None:
     print("❌ ERROR: Render dashboard mein DATABASE_URL missing hai!")
